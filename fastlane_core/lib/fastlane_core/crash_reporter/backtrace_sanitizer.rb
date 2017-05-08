2f806e2a0214176c63ea67a31d725df6c96da19a
David Ohayon <ohayon.1@gmail.com>
2017-05-08 17:06:12 -0400

    add preliminary santization

--- fastlane_core/lib/fastlane_core/crash_reporter/backtrace_sanitizer.rb
+++ fastlane_core/lib/fastlane_core/crash_reporter/backtrace_sanitizer.rb
@@ -7,9 +7,41 @@ module FastlaneCore
         # up to the point where the crash was initiated.
         # The two stack frames we are dropping are `method_missing` and
         # the call to `crash!` or `user_error!`.
-        backtrace.drop(2)
+        stack = backtrace.drop(2)
       else
-        backtrace
+        stack = backtrace
+      end
+
+      stack = remove_fastlane_gem_path(backtrace: stack)
+      stack = remove_gem_home_path(backtrace: stack)
+      stack = remove_home_dir_mentions(backtrace: stack)
+    end
+
+    def self.remove_fastlane_gem_path(backtrace: nil)
+      fastlane_path = Gem.loaded_specs['fastlane'].full_gem_path
+      backtrace.map do |frame|
+        if frame.include?(fastlane_path)
+          frame.gsub!(fastlane_path, '')
+        end
+        frame
+      end
+    end
+
+    def self.remove_gem_home_path(backtrace: nil)
+      backtrace.map do |frame|
+        if frame.include?(ENV['GEM_HOME'])
+          frame.gsub!(ENV['GEM_HOME'], 'GEM_HOME')
+        end
+        frame
+      end
+    end
+
+    def self.remove_home_dir_mentions(backtrace: nil)
+      backtrace.map do |frame|
+        if frame.include?(Dir.home)
+          frame.gsub!(Dir.home, '~')
+        end
+        frame
       end
     end
 
